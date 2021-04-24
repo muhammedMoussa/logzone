3b91c7eb20717b908b7be64df2fb39762b0bdbbc
muhammedMoussa <muhammedmoussa@hotmail.com>
2021-04-24 03:18:38 +0200

    refactor(src): rollback minimal structure

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-const log = (msg) => console.log(msg);
+const log = (msg, color = "", background = "", fontSize = "") => {
+  console.log(`%c${msg} `, `background: ${background}; color: ${color};  font-size: ${fontSize}px; padding: 2.74px; border-radius: 3.74px`);
+};
+
 
 module.exports = {
   log,
